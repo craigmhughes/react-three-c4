b71af1e629dc9bd96269a8049a740d3ed01a4eae
Craig Hughes <hghscraig@gmail.com>
2019-11-09 23:26:31 +0000

    Able to select active column

--- src/components/game.js
+++ src/components/game.js
@@ -39,11 +39,13 @@ const Board = ({active, setActive, isMoving, setIsMoving})=>{
   // Width, Height, Depth
   let dimensions =  [1.5, 1.5, 0.2]
   let colNum = 7;
-
   let colElements = [];
+  
+  // Set active column, start at -1 and none selected.
+  const [activeCol, setActiveCol] = useState(-1);
 
   for(let i = 0; i < colNum; i++){
-    colElements.push(<BoardColumn key={i} id={i} dimensions={dimensions} cols={colNum}/>)
+    colElements.push(<BoardColumn key={i} id={i} dimensions={dimensions} cols={colNum} activeCol={activeCol} setActiveCol={setActiveCol}/>)
   }
 
   useFrame(()=>{
@@ -57,27 +59,29 @@ const Board = ({active, setActive, isMoving, setIsMoving})=>{
       ref={ref}
       rotation-y={props.rotationY}
       position-x={0.05}>
-        <boxGeometry attach="geometry" args={dimensions} />
-        <a.meshPhysicalMaterial attach="material" color="red"/>
+        {/* <boxGeometry attach="geometry" args={dimensions} />
+        <a.meshPhysicalMaterial attach="material" color="red"/> */}
         {colElements}
     </a.mesh>
   )
 }
 
-const BoardColumn = ({id, dimensions, cols})=>{
+const BoardColumn = ({id, dimensions, cols, activeCol, setActiveCol})=>{
   const ref = useRef();
-  // const props = useSpring({
-    
-  // });
+
+  const props = useSpring({
+    color: activeCol === id ? "red" : "grey",
+  });
 
   return (
     <a.mesh
       ref={ref}
       position-x={-dimensions[0]/2 + ((dimensions[0] / cols) + 0.02) * id }
-      position-z={0}>
+      position-z={0.1}
+      onClick={()=>{setActiveCol(id)}}>
       
       <boxGeometry attach="geometry" args={[(dimensions[0] / cols), dimensions[1], dimensions[2]]} />
-      <a.meshPhysicalMaterial attach="material" color="grey"/>
+      <a.meshPhysicalMaterial attach="material" color={props.color}/>
     </a.mesh>
   )
 }
