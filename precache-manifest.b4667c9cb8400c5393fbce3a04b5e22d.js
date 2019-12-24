b6267854dc1d1d4db76ee53ecbf579489fb6f10d
Craig Hughes <hghscraig@gmail.com>
2019-12-24 07:07:03 +0000

    Updates

---
+++ precache-manifest.b4667c9cb8400c5393fbce3a04b5e22d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fc038762f36d5fffd5e00e541ffdb1",
+    "url": "/react-three-c4/index.html"
+  },
+  {
+    "revision": "c760b62b9b8a08d98687",
+    "url": "/react-three-c4/static/css/main.dd20dd7b.chunk.css"
+  },
+  {
+    "revision": "57e6ab06a2c25fb0896c",
+    "url": "/react-three-c4/static/js/2.08b9d2ca.chunk.js"
+  },
+  {
+    "revision": "c760b62b9b8a08d98687",
+    "url": "/react-three-c4/static/js/main.cd4b0160.chunk.js"
+  },
+  {
+    "revision": "91ad5e293d5c5456bc64",
+    "url": "/react-three-c4/static/js/runtime-main.947a9df9.js"
+  }
+]);
